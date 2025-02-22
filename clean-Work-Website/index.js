895e7a0a3116f2615e3320e1aefe84eb4d45a330
Elechia Juma <otienoelechia3@gmail.com>
2025-02-22 15:03:15 +0300

    styling the hero, navbar and landing page

--- clean-Work-Website/index.js
+++ clean-Work-Website/index.js
@@ -1,9 +1,19 @@
-const hero = ["images/images/slideshow/afro1.jpg", "images/images/slideshow/afro2.jpg", "images/images/slideshow/afro3.jpg"];
-        let index = 0;
+     const images = [
+            "images/images/slideshow/afro1.jpg", "images/images/slideshow/afro2.jpg", "images/images/slideshow/afro3.jpg"
+        ];
 
-        function changeImage() {
-            index = (index + 1) % hero.length;
-            document.getElementById("slide").src = [index];
+        let currentIndex = 0;
+        const slider = document.querySelector(".background-slider");
+
+        function changeBackground() {
+            slider.style.backgroundImage = `url(${images[currentIndex]})`;
+            currentIndex = (currentIndex + 1) % images.length;
         }
 
-        setInterval(changeImage, 3000); // Change image every 3 seconds
+        // Initial background setup
+        changeBackground();
+
+        // Change background every 3 seconds
+        setInterval(changeBackground, 3000);
+
+
